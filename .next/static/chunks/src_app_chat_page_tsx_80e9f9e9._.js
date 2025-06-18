3d4df70ba8f3771b4a9160eef93803814037ba7a
dinhvien04 <nguyendinhvien2004@gmail.com>
2025-06-18 17:07:50 +0700

    Add .next/static/chunks/src_app_chat_page_tsx_80e9f9e9._.js 1750241269696

---
+++ .next/static/chunks/src_app_chat_page_tsx_80e9f9e9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ef444ccd._.js",
+  "static/chunks/src_49c6971c._.js"
+],
+    source: "dynamic"
+});
